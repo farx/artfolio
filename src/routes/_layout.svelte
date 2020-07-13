727990db80f61457137edd2f2c811d79e6997a95
Max Gnipping <max@farx.io>
2020-07-13 23:42:14 +0200

    Removed the preview function for now

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,20 +3,9 @@
 	import Footer from '../components/Footer.svelte';
 
 	export let segment;
-	export let api;
-
-	api = process.env.SAPPER_APP_PRISMIC_API;
 </script>
 
 
-<svelte:head>
-	<script>
-	window.prismic = {
-		endpoint: { api }
-	};
-	</script>
-	<script type="text/javascript" src="https://static.cdn.prismic.io/prismic.min.js?new=true"></script>
-</svelte:head>
 <Nav { segment } />
 
 <main>
