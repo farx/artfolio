b1408bbfa2919157a2d75b1b576be058e8098725
Max Gnipping <max@farx.io>
2020-07-13 23:35:15 +0200

    Attempt to fix preview link

--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -3,8 +3,20 @@
 	import Footer from '../components/Footer.svelte';
 
 	export let segment;
+	export let api;
+
+	api = process.env.SAPPER_APP_PRISMIC_API;
 </script>
 
+
+<svelte:head>
+	<script>
+	window.prismic = {
+		endpoint: { api }
+	};
+	</script>
+	<script type="text/javascript" src="https://static.cdn.prismic.io/prismic.min.js?new=true"></script>
+</svelte:head>
 <Nav { segment } />
 
 <main>
