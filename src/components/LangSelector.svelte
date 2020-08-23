e0685774278f77b70f37b8ebb1d6cf176bad090d
Max Gnipping <max@farx.io>
2020-08-23 17:53:45 +0200

    Add forgotten selector

---
+++ src/components/LangSelector.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { lang } from "../routes/_settings.js";
+
+</script>
+
+<style>
+ .langselector {
+     position: absolute;
+     right: 0;
+
+     padding: 0 1em;
+     background-color: rgba(255,255,255,0.6);
+ }
+
+ .langselector a {
+     font-size: 0.8em;
+     line-height: 1.8;
+ }
+</style>
+
+<div class="langselector-wrapper">
+<div class="langselector">
+    {#each $lang.translations as translation, i (translation.url)}
+        {#if translation.code == $lang.current.name }
+            <b><a href="/{translation.url}">{ translation.code }</a></b>
+        {:else}
+            <a href="/{translation.url}">{ translation.code }</a>
+        {/if}
+    {/each}
+</div>
+</div>
